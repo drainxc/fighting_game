fb4ae13d32fa88ceecf8e7392490f38adeecad49
eastcopper <ldh7228@gmail.com>
2022-04-11 10:13:27 +0900

    📄 :: (#5)[Canvas] Add Player

--- src/components/page/canvas/index.tsx
+++ src/components/page/canvas/index.tsx
@@ -7,16 +7,34 @@ export default function Canvas() {
 
   useEffect(() => {
     const canvas: any = canvasRef.current;
+    const ctx = canvas.getContext("2d");
 
     if (!canvas) return;
 
     canvas.width = window.innerWidth - 1;
     canvas.height = window.innerHeight - 1;
 
-    const ctx = canvas.getContext("2d");
-
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctxRef.current = ctx;
+
+    type PositionType = {
+      x?: number;
+      y?: number;
+    };
+
+    class Sprite {
+      position: PositionType;
+      constructor(position: any) {
+        console.log(position);
+        this.position = position;
+      }
+    }
+
+    const player = new Sprite({
+      x: 0,
+      y: 0,
+    });
+    console.log(player);
 
     setCtx(ctxRef.current);
   }, []);
