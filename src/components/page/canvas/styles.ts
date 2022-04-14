428b7bd0297d3f5997caf7fb5b4091d91f3a245a
eastcopper <ldh7228@gmail.com>
2022-04-14 15:52:31 +0900

    ⚡ :: (#25)[Canvas] Design Canvas

--- src/components/page/canvas/styles.ts
+++ src/components/page/canvas/styles.ts
@@ -3,3 +3,35 @@ import styled from "styled-components";
 export const MainDiv = styled.div`
   overflow: hidden;
 `;
+
+export const HealthBar = styled.div`
+  position: absolute;
+  display: flex;
+  width: 100%;
+  align-items: center;
+  padding: 50px;
+  box-sizing: border-box;
+`;
+
+export const PlayerBar = styled.div`
+  position: relative;
+  height: 50px;
+  width: 100%;
+`;
+
+export const EnemyBar = styled.div`
+  position: relative;
+  height: 50px;
+  width: 100%;
+`;
+
+export const Timer = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: blue;
+  flex-shrink: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+`;
