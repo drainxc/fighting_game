82200388502152476cd43dc56b6fc8ae82cda3f9
eastcopper <ldh7228@gmail.com>
2022-04-18 19:10:39 +0900

    📑 :: (#48)[Data] Add Image

---
+++ src/lib/export/data.ts
@@ -0,0 +1,26 @@
+import warriorIdle from "../../asset/img/warriorSprite/Idle.png";
+import warriorRun from "../../asset/img/warriorSprite/Run.png";
+import warriorJump from "../../asset/img/warriorSprite/Jump.png";
+import warriorFall from "../../asset/img/warriorSprite/Fall.png";
+import warriorAttack1 from "../../asset/img/warriorSprite/Attack1.png";
+import wizardIdle from "../../asset/img/wizardSprite/Idle.png";
+import wizardRun from "../../asset/img/wizardSprite/Run.png";
+import wizardJump from "../../asset/img/wizardSprite/Jump.png";
+import wizardFall from "../../asset/img/wizardSprite/Fall.png";
+import wizardAttack1 from "../../asset/img/wizardSprite/Attack1.png";
+
+export const warriorImg = {
+  warriorIdle: warriorIdle,
+  warriorRun: warriorRun,
+  warriorJump: warriorJump,
+  warriorFall: warriorFall,
+  warriorAttack1: warriorAttack1,
+};
+
+export const wizardImg = {
+  wizardIdle: wizardIdle,
+  wizardRun: wizardRun,
+  wizardJump: wizardJump,
+  wizardFall: wizardFall,
+  wizardAttack1: wizardAttack1,
+};
