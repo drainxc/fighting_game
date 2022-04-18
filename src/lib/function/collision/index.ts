45bd2f0c59b1e53aa7f0079fe16e71d124f461e5
eastcopper <ldh7228@gmail.com>
2022-04-18 14:25:20 +0900

    ⚡ :: (#38)[Collision] Update Attack

--- src/lib/function/collision/index.ts
+++ src/lib/function/collision/index.ts
@@ -1,9 +1,10 @@
-export function collision(p: any, e: any) {
-    return (
-      p.range.width + p.range.position.x >= e.position.x &&
-      p.range.position.x <= e.position.x + e.width &&
-      p.range.position.y + p.range.height >= e.position.y &&
-      p.range.position.y <= e.position.y + e.height &&
-      p.attacking
-    );
-  }
+export function collision(p: any, e: any, a: boolean) {
+  return (
+    p.range.width + p.range.position.x >= e.position.x &&
+    p.range.position.x <= e.position.x + e.width &&
+    p.range.position.y + p.range.height >= e.position.y &&
+    p.range.position.y <= e.position.y + e.height &&
+    a &&
+    p.framecurrent === 4
+  );
+}
