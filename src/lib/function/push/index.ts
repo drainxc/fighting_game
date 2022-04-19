2fbf120f5f02c2ba176d9b5e4c90587ce02ae2fb
eastcopper <ldh7228@gmail.com>
2022-04-20 08:22:11 +0900

    ⚡ :: (#82)[Push] Update Push

--- src/lib/function/push/index.ts
+++ src/lib/function/push/index.ts
@@ -1,9 +1,12 @@
 export function push(p: any, e: any) {
-  if (
-    !(e.position.x > p.position.x + 80) ||
-    !(p.position.x < e.position.x - 80)
-  ) {
-    e.position.x += 9;
+  if (!(p.position.x < -(e.position.x + 200))) {
+    e.position.x -= 9;
     p.position.x -= 9;
+  }
+  if (p.position.x < 10) {
+    p.position.x = 10;
+  }
+  if (e.position.x < -1900) {
+    e.position.x = -1900;
   }
 }
