92daa2e439d7f27b51a07a20dae63207da0b74a5
eastcopper <ldh7228@gmail.com>
2022-04-20 08:21:00 +0900

    ⚡ :: (#79)[Collision] Update Collision

--- src/lib/function/collision/index.ts
+++ src/lib/function/collision/index.ts
@@ -1,9 +1,8 @@
 export function collision(p: any, e: any, a: boolean, f: number) {
   return (
-    p.range.width + p.range.position.x >= e.position.x &&
-    p.range.position.x <= e.position.x + e.width &&
-    p.range.position.y + p.range.height >= e.position.y &&
-    p.range.position.y <= e.position.y + e.height &&
+    p.range.width - p.range.position.x < e.position.x + e.width &&
+    p.position.y + p.range.height >= e.position.y &&
+    p.position.y <= e.position.y + e.range.height &&
     a &&
     p.framecurrent === f
   );
