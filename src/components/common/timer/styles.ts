4ec04fe76ed3bc375dedac3ca1910aee0cf1cbab
eastcopper <ldh7228@gmail.com>
2022-04-19 10:20:22 +0900

    ⚡ :: (#61)[Timer] Design Timer

---
+++ src/components/common/timer/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Time = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: blue;
+  flex-shrink: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+`;
