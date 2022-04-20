b60e6118a60e76d5625019bca9e5d6a9fd829357
eastcopper <ldh7228@gmail.com>
2022-04-20 09:21:47 +0900

    ⚡ :: (#86)[Data] Add hit

--- src/lib/export/data.ts
+++ src/lib/export/data.ts
@@ -1,6 +1,9 @@
 import bot1Idle from "../../asset/img/toaster/idle.png";
 import bot1Attack from "../../asset/img/toaster/attack.png";
 import bot1Run from "../../asset/img/toaster/run.png";
+import bot2Idle from "../../asset/img/Shock Sweeper/wake2.png";
+import bot2Attack from "../../asset/img/Shock Sweeper/attackCombo.png";
+import bot2Run from "../../asset/img/Shock Sweeper/shuffe(move).png";
 
 interface PlayerAsset {
   idle: string;
@@ -26,22 +29,46 @@ interface Keycap {
 interface Frame {
   delay: number;
   atkFrame: number;
-  jumpFrame: number;
-  fallFrame: number;
   moveFrame: number;
   idleFrame: number;
 }
 
 export const playerImg: PlayerAsset = {
-  idle: bot1Idle,
-  run: bot1Run,
-  attack1: bot1Attack,
+  idle: bot2Idle,
+  run: bot2Run,
+  attack1: bot2Attack,
 };
 
 export const enemyImg: PlayerAsset = {
-  idle: bot1Idle,
-  run: bot1Run,
-  attack1: bot1Attack,
+  idle: bot2Idle,
+  run: bot2Run,
+  attack1: bot2Attack,
+};
+
+export const pHit = {
+  width: -240,
+  height: 200,
+  hitFrame: 6,
+};
+
+export const eHit = {
+  width: -240,
+  height: 200,
+  hitFrame: 6,
+};
+
+export const pframe: Frame = {
+  delay: 6,
+  atkFrame: 27,
+  moveFrame: 8,
+  idleFrame: 1,
+};
+
+export const eframe: Frame = {
+  delay: 6,
+  atkFrame: 27,
+  moveFrame: 8,
+  idleFrame: 1,
 };
 
 export const ekey: Key = {
@@ -71,22 +98,4 @@ export const ekeycap: Keycap = {
   a: "ArrowRight",
   d: "ArrowLeft",
   attack: "7",
-};
-
-export const pframe: Frame = {
-  delay: 6,
-  atkFrame: 11,
-  jumpFrame: 8,
-  fallFrame: 8,
-  moveFrame: 8,
-  idleFrame: 5,
-};
-
-export const eframe: Frame = {
-  delay: 6,
-  atkFrame: 11,
-  jumpFrame: 8,
-  fallFrame: 8,
-  moveFrame: 8,
-  idleFrame: 5,
 };
