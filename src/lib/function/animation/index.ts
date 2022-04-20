0a4077cdba6937fcade3d1e115c75b845928cf8d
eastcopper <ldh7228@gmail.com>
2022-04-20 17:56:00 +0900

    ⚡ :: (#95)[Animation] Update Animation

--- src/lib/function/animation/index.ts
+++ src/lib/function/animation/index.ts
@@ -1,11 +1,25 @@
 export function animation(k: any, player: any, img: any, frame: any) {
-  const { attack1, run, idle } = img;
-  const { atkFrame, moveFrame, idleFrame } = frame;
-  if (k.attack) {
+  const { attack, run, idle, beShot, death } = img;
+  const { atkFrame, moveFrame, idleFrame, deathframe } = frame;
+  if (k.beShot) {
+    player.frame = 2;
+    player.speed.x = 0;
+    player.image.src = beShot;
+    setTimeout(() => {
+      k.beShot = false;
+    }, 200);
+  } else if (k.death) {
+    player.frame = deathframe;
+    player.image.src = death;
+    setTimeout(() => {
+      player.delay = 0;
+      player.framecurrent = deathframe - 1;
+    }, deathframe * 100);
+  } else if (k.attack) {
     player.delay = 6;
     player.frame = atkFrame;
     player.speed.x = 0;
-    player.image.src = attack1;
+    player.image.src = attack;
   } else if (k.move) {
     player.frame = moveFrame;
     player.image.src = run;
