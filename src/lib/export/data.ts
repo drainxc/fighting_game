d2bea6403b720f0de0df476002b229d442315f97
eastcopper <ldh7228@gmail.com>
2022-04-20 22:31:14 +0900

    ⚰ :: (#102)[Asset] Drop Toaster

--- src/lib/export/data.ts
+++ src/lib/export/data.ts
@@ -1,6 +1,10 @@
-// import bot1Idle from "../../asset/img/toaster/idle.png";
-// import bot1Attack from "../../asset/img/toaster/attack.png";
-// import bot1Run from "../../asset/img/toaster/run.png";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import bot1Idle from "../../asset/img/Bot Wheel/static idle.png";
+import bot1Attack from "../../asset/img/Bot Wheel/attack.png";
+import bot1Run from "../../asset/img/Bot Wheel/move with FX.png";
+import bot1BeShot from "../../asset/img/Bot Wheel/damaged.png";
+import bot1Death from "../../asset/img/Bot Wheel/death.png";
 import bot2Idle from "../../asset/img/Shock Sweeper/wake2.png";
 import bot2Attack from "../../asset/img/Shock Sweeper/attackCombo.png";
 import bot2Run from "../../asset/img/Shock Sweeper/shuffe(move).png";
@@ -16,17 +20,11 @@ import bot4Attack from "../../asset/img/Ball and Chain Bot/attack.png";
 import bot4Run from "../../asset/img/Ball and Chain Bot/run.png";
 import bot4BeShot from "../../asset/img/Ball and Chain Bot/hit.png";
 import bot4Death from "../../asset/img/Ball and Chain Bot/death.png";
-import bot5Idle from "../../asset/img/Eye ball Monster/idle.png"
-import bot5Attack from "../../asset/img/Eye ball Monster/attack.png"
-import bot5Run from "../../asset/img/Eye ball Monster/run.png"
-import bot5BeShot from "../../asset/img/Eye ball Monster/hit.png"
-import bot5Death from "../../asset/img/Eye ball Monster/death.png"
-import bot6Idle from "../../asset/img/Bot Wheel/static idle.png"
-import bot6Attack from "../../asset/img/Bot Wheel/attack.png"
-import bot6Run from "../../asset/img/Bot Wheel/move with FX.png"
-import bot6BeShot from "../../asset/img/Bot Wheel/damaged.png"
-import bot6Death from "../../asset/img/Bot Wheel/death.png"
-
+import bot5Idle from "../../asset/img/Eye ball Monster/idle.png";
+import bot5Attack from "../../asset/img/Eye ball Monster/attack.png";
+import bot5Run from "../../asset/img/Eye ball Monster/run.png";
+import bot5BeShot from "../../asset/img/Eye ball Monster/hit.png";
+import bot5Death from "../../asset/img/Eye ball Monster/death.png";
 
 interface PlayerAsset {
   idle: string;
@@ -57,12 +55,13 @@ interface Data {
   width: number;
   height: number;
   hittime: number;
-  hitframe: number[];
+  hitFrame: number[];
   atkFrame: number;
   moveFrame: number;
   idleFrame: number;
   damaged: number;
-  deathframe: number;
+  deathFrame: number;
+  cooltime: number;
 }
 
 export const playerImg: PlayerAsset = {
@@ -74,95 +73,102 @@ export const playerImg: PlayerAsset = {
 };
 
 export const enemyImg: PlayerAsset = {
-  idle: bot6Idle,
-  run: bot6Run,
-  attack: bot6Attack,
-  beShot: bot6BeShot,
-  death: bot6Death,
+  idle: bot1Idle,
+  run: bot1Run,
+  attack: bot1Attack,
+  beShot: bot1BeShot,
+  death: bot1Death,
+};
+
+const bot1Data: Data = {
+  width: -650,
+  height: 200,
+  hittime: 2,
+  hitFrame: [1, 8],
+  atkFrame: 11,
+  moveFrame: 8,
+  idleFrame: 1,
+  damaged: 1,
+  deathFrame: 6,
+  cooltime: 5,
 };
 
 const bot2Data: Data = {
   width: -345,
   height: 200,
   hittime: 3,
-  hitframe: [6, 14, 22],
+  hitFrame: [6, 14, 22],
   atkFrame: 27,
   moveFrame: 8,
   idleFrame: 1,
   damaged: 1,
-  deathframe: 5,
+  deathFrame: 5,
+  cooltime: 5,
 };
 
 const bot3Data: Data = {
   width: -610,
   height: 100,
   hittime: 1,
-  hitframe: [4],
+  hitFrame: [4],
   atkFrame: 7,
   moveFrame: 6,
   idleFrame: 5,
   damaged: 1,
-  deathframe: 8,
+  deathFrame: 8,
+  cooltime: 5,
 };
 
 const bot4Data: Data = {
   width: -320,
   height: 200,
   hittime: 2,
-  hitframe: [2, 6],
+  hitFrame: [2, 6],
   atkFrame: 9,
   moveFrame: 8,
   idleFrame: 5,
   damaged: 1,
-  deathframe: 5,
+  deathFrame: 5,
+  cooltime: 5,
 };
 
 const bot5Data: Data = {
   width: -480,
   height: 200,
   hittime: 1,
-  hitframe: [6],
+  hitFrame: [6],
   atkFrame: 13,
   moveFrame: 7,
   idleFrame: 7,
   damaged: 1,
-  deathframe: 12,
+  deathFrame: 12,
+  cooltime: 5,
 };
 
-const bot6Data: Data = {
-  width: -650,
-  height: 200,
-  hittime: 2,
-  hitframe: [1, 8],
-  atkFrame: 11,
-  moveFrame: 8,
-  idleFrame: 1,
-  damaged: 1,
-  deathframe: 6,
-};
-
-export const pHit = {
+export const pHit: Data = {
   width: bot2Data.width,
   height: bot2Data.height,
   hittime: bot2Data.hittime,
-  hitFrame: bot2Data.hitframe,
+  hitFrame: bot2Data.hitFrame,
   atkFrame: bot2Data.atkFrame,
   moveFrame: bot2Data.moveFrame,
   idleFrame: bot2Data.idleFrame,
   damaged: bot2Data.damaged,
-  deathframe: bot2Data.deathframe
+  deathFrame: bot2Data.deathFrame,
+  cooltime: bot2Data.cooltime,
 };
 
-export const eHit = {
-  width: bot6Data.width,
-  height: bot6Data.height,
-  hittime: bot6Data.hittime,
-  hitFrame: bot6Data.hitframe,
-  atkFrame: bot6Data.atkFrame,
-  moveFrame: bot6Data.moveFrame,
-  idleFrame: bot6Data.idleFrame,
-  damaged: bot6Data.damaged,
-  deathframe: bot6Data.deathframe
+export const eHit: Data = {
+  width: bot1Data.width,
+  height: bot1Data.height,
+  hittime: bot1Data.hittime,
+  hitFrame: bot1Data.hitFrame,
+  atkFrame: bot1Data.atkFrame,
+  moveFrame: bot1Data.moveFrame,
+  idleFrame: bot1Data.idleFrame,
+  damaged: bot1Data.damaged,
+  deathFrame: bot1Data.deathFrame,
+  cooltime: bot1Data.cooltime,
 };
 
 export const ekey: Key = {
