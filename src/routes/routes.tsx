3279b9efa83285a20d3ee9c38c0ca5579e8ecd2b
eastcopper <ldh7228@gmail.com>
2022-04-21 20:01:14 +0900

    📄 :: (#114)[Select] Add Select

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,9 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Canvas from "../components/page/canvas";
+import Select from "../components/page/selectPage";
 
 export default function Router() {
   return (
     <>
-      <Canvas />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Select />} />
+          <Route path="/game:id" element={<Canvas />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
