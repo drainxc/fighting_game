7843333c349a25c9962237e712ddd095ff39cc8f
eastcopper <ldh7228@gmail.com>
2022-04-21 11:17:41 +0900

    ⚡ :: (#107)[HealthBar] Design HealthBar

---
+++ src/components/common/healthbar/styles.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const HealthBar = styled.div`
+  position: absolute;
+  display: flex;
+  width: 100%;
+  align-items: center;
+  padding: 50px;
+  box-sizing: border-box;
+`;
+
+export const PlayerBar = styled.div`
+  position: relative;
+  height: 50px;
+  width: 100%;
+`;
+
+export const EnemyBar = styled.div`
+  position: relative;
+  height: 50px;
+  width: 100%;
+`;
+
+export const Barbg = styled.div`
+  background-color: yellow;
+  height: 50px;
+`;
