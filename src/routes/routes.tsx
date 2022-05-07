e346bb98161c1deaaa6106c87865471ef3a323e3
eastcopper <ldh7228@gmail.com>
2022-05-07 23:57:23 +0900

    ⚡ :: (#147)[Routes] Update Routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Canvas from "../components/page/canvas";
 import Select from "../components/page/selectPage";
+import Title from "../components/page/title";
 
 export default function Router() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Select />} />
+          <Route path="/" element={<Title />} />
+          <Route path="/select" element={<Select />} />
           <Route path="/game:id" element={<Canvas />} />
         </Routes>
       </BrowserRouter>
