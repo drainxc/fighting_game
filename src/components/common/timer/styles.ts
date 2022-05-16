6e908300c176483d57cac208c738ea9f601424b1
eastcopper <ldh7228@gmail.com>
2022-05-17 08:23:50 +0900

    ⚡ :: (#175)[Timer] Update Font

--- src/components/common/timer/styles.ts
+++ src/components/common/timer/styles.ts
@@ -3,8 +3,13 @@ import styled from "styled-components";
 export const Time = styled.div`
   width: 100px;
   height: 100px;
-  background-color: blue;
+  font-family: "Press Start 2P", cursive;
   flex-shrink: 0;
+  font-size: 45px;
+  text-align: center;
+  margin-left: 5px;
+  margin-bottom: 20px;
+  text-shadow: -6px 0 #313131, 0 6px #313131, 6px 0 #313131, 0 -6px #313131;
   display: flex;
   justify-content: center;
   align-items: center;
