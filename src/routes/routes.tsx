6c75756f157765e54723ad9a1443d7f6c5a7806b
eastcopper <ldh7228@gmail.com>
2022-05-18 19:48:36 +0900

    ⚡ :: (#192)[Router] Add help

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Canvas from "../components/page/canvas";
+import Help from "../components/page/help";
 import Select from "../components/page/selectPage";
 import Title from "../components/page/title";
 
@@ -10,6 +11,7 @@ export default function Router() {
         <Routes>
           <Route path="/" element={<Title />} />
           <Route path="/select" element={<Select />} />
+          <Route path="/help" element={<Help />} />
           <Route path="/game:id" element={<Canvas />} />
         </Routes>
       </BrowserRouter>
