63cfbf44241616a62f9401044eb73427c89fa767
eastcopper <ldh7228@gmail.com>
2022-05-18 16:08:30 +0900

    ⚡ :: (#181)[HealthBar] Design HealthBar

--- src/components/common/healthbar/styles.ts
+++ src/components/common/healthbar/styles.ts
@@ -5,26 +5,31 @@ export const HealthBar = styled.div`
   position: absolute;
   display: flex;
   width: 100%;
-  align-items: center;
   padding: 50px 100px;
   box-sizing: border-box;
-  span {
-    margin-top: 16px;
-    position: absolute;
-    background: red;
-    height: 19px;
-    top: 0;
-    bottom: 0;
-  }
 `;
 
-export const PlayerBar = styled.div`
+export const Health = styled.div`
+  position: absolute;
+  background: red;
+  height: 19px;
+  top: 42px;
+`;
+
+export const Energe = styled.div`
+  position: absolute;
+  background: yellowgreen;
+  height: 10px;
+  bottom: 23px;
+`;
+
+export const Bar = styled.div`
   position: relative;
-  height: 50px;
+  height: 100px;
   width: 100%;
 `;
 
 export const Barbg = styled.div`
   background-image: url(${health});
-  height: 50px;
+  height: 100px;
 `;
